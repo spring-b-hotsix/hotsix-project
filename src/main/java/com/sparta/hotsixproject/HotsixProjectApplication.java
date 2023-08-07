44ad0a3508337a5c2a8bbae370e0d339e127f18b
Haena <johanna0123@naver.com>
2023-08-07 14:13:41 +0900

    application security 제외 설정

--- src/main/java/com/sparta/hotsixproject/HotsixProjectApplication.java
+++ src/main/java/com/sparta/hotsixproject/HotsixProjectApplication.java
@@ -2,8 +2,11 @@ package com.sparta.hotsixproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@SpringBootApplication
+@EnableJpaAuditing
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class HotsixProjectApplication {
 
 	public static void main(String[] args) {
