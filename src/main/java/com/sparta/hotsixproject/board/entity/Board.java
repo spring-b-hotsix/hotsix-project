2deb9bdcb4076aca693046428da6e44448299bfc
Jo Woojin <dnwls3525@gmail.com>
2023-08-07 21:22:16 +0900

    [Init]엔티티 추가(Board, BoardUser)

--- src/main/java/com/sparta/hotsixproject/board/entity/Board.java
+++ src/main/java/com/sparta/hotsixproject/board/entity/Board.java
@@ -1,4 +1,46 @@
 package com.sparta.hotsixproject.board.entity;
 
+import com.sparta.hotsixproject.user.entity.User;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.awt.*;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@Table(name = "boards")
 public class Board {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+
+    @Column(nullable=false)
+    private String name;
+
+    @Column
+    private String description;
+
+    @Column
+    private Color color;
+
+    @ManyToOne
+    @JoinColumn(name="user_id", nullable = false)
+    private User user;
+
+
+    public Board(String name, String description, Color color){
+        this.name = name;
+        this.description = description;
+        this.color = color;
+    }
+
+    public void modifyColor(Color color){
+        this.color = color;
+    }
+
+
+
+
 }
