48adf80d0fec2956cb35c5228060b8a984ca9f04
hyukjin <cdh5771@naver.com>
2023-08-09 11:49:03 +0900

    [Update] ResponseDto에 userId 추가

--- src/main/java/com/sparta/hotsixproject/board/dto/MemberResponseDto.java
+++ src/main/java/com/sparta/hotsixproject/board/dto/MemberResponseDto.java
@@ -6,10 +6,12 @@ import lombok.Getter;
 
 @Getter
 public class MemberResponseDto {
+    private Long userId;
     private String nickname;
     private String email;
 
     public MemberResponseDto(BoardUser boardUser) {
+        this.userId = boardUser.getUser().getId();
         this.nickname = boardUser.getUser().getNickname();
         this.email = boardUser.getUser().getEmail();
     }
