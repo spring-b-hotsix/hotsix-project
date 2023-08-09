cad316a0332a9a86593633fb83962ee685891459
jungeun-choi <jungeun5.choi@gmail.com>
2023-08-10 00:48:06 +0900

    [Complete] swagger 추가: CardController

--- src/main/java/com/sparta/hotsixproject/card/controller/CardController.java
+++ src/main/java/com/sparta/hotsixproject/card/controller/CardController.java
@@ -4,6 +4,10 @@ import com.sparta.hotsixproject.card.dto.*;
 import com.sparta.hotsixproject.card.service.CardService;
 import com.sparta.hotsixproject.common.advice.ApiResponseDto;
 import com.sparta.hotsixproject.common.security.UserDetailsImpl;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -14,80 +18,122 @@ import java.util.List;
 @RestController
 @RequestMapping("/boards")
 @RequiredArgsConstructor
+@Tag(name = "카드 관련 API", description = "카드 관련 API 입니다.")
 public class CardController {
-
     public final CardService cardService;
 
-    // 카드 생성
     @PostMapping("/{boardId}/sides/{sideId}/cards")
-    public ResponseEntity<ApiResponseDto> createCard(@PathVariable Long boardId, @PathVariable Long sideId,
-                                                     @RequestBody CardRequestDto requestDto, @AuthenticationPrincipal UserDetailsImpl userDetails) {
+    @Operation(summary = "카드 생성", description = "@PathVariable을 통해 boardId와 sideId를 받아와, 해당 위치에 카드를 생성합니다. Dto를 통해 name(이름) 값을 받아와 카드를 생성할 때 해당 name을 저장합니다.")
+    public ResponseEntity<ApiResponseDto> createCard(
+            @Parameter(name = "boardId", description = "카드를 생성할 board의 id", in = ParameterIn.PATH) @PathVariable Long boardId,
+            @Parameter(name = "sideId", description = "카드를 생성할 side(컬럼)의 id", in = ParameterIn.PATH) @PathVariable Long sideId,
+            @Parameter(name = "requestDto", description = "카드 생성시 필요한 정보 (name)") @RequestBody CardRequestDto requestDto,
+            @AuthenticationPrincipal UserDetailsImpl userDetails
+    ) {
         return cardService.createCard(sideId, requestDto.getName(), userDetails.getUser());
     }
 
-    // 카드 상세 조회
     @GetMapping("/{boardId}/sides/{sideId}/cards/{cardId}")
-    public ResponseEntity<CardResponseDto> getCard(@PathVariable Long boardId, @PathVariable Long sideId, @PathVariable Long cardId) {
+    @Operation(summary = "카드 상세 조회", description = "@PathVariable을 통해 boardId, sideId, cardId를 받아와, 해당 정보와 일치하는 카드 1개의 상세 정보를 조회합니다.")
+    public ResponseEntity<CardResponseDto> getCard(
+            @Parameter(name = "boardId", description = "선택한 카드가 위치한 board의 id", in = ParameterIn.PATH) @PathVariable Long boardId,
+            @Parameter(name = "sideId", description = "선택한 카드가 위치한 side(컬럼)의 id", in = ParameterIn.PATH) @PathVariable Long sideId,
+            @Parameter(name = "cardId", description = "상세 정보를 조회할 card의 id", in = ParameterIn.PATH) @PathVariable Long cardId
+    ) {
         return cardService.getCard(boardId, sideId, cardId);
     }
 
-    // side 안의 카드 전체 조회
     @GetMapping("/{boardId}/sides/{sideId}/cards")
-    public List<CardResponseDto> getCards(@PathVariable Long boardId, @PathVariable Long sideId) {
+    @Operation(summary = "카드 전체 조회", description = "@PathVariable을 통해 boardId, sideId를 받아와, 해당 정보와 일치하는 모든 카드의 정보를 조회합니다.")
+    public List<CardResponseDto> getCards(
+            @Parameter(name = "boardId", description = "선택한 카드가 위치한 board의 id", in = ParameterIn.PATH) @PathVariable Long boardId,
+            @Parameter(name = "sideId", description = "선택한 카드가 위치한 side의 id", in = ParameterIn.PATH) @PathVariable Long sideId
+    ) {
         return cardService.getCards(boardId, sideId);
     }
 
-    // 카드 작업자 추가
     @PostMapping("/{boardId}/sides/{sideId}/cards/{cardId}/worker")
-    public ResponseEntity<CardResponseDto> addWorker(@PathVariable Long boardId, @PathVariable Long sideId, @PathVariable Long cardId, @RequestBody InviteCardRequestDto requestDto) {
+    @Operation(summary = "카드 작업자 추가", description = "@PathVariable을 통해 boardId, sideId, cardId를 받아와, 해당 정보와 일치하는 카드에 작업자를 추가합니다.")
+    public ResponseEntity<CardResponseDto> addWorker(
+            @Parameter(name = "boardId", description = "선택한 카드가 위치한 board의 id", in = ParameterIn.PATH) @PathVariable Long boardId,
+            @Parameter(name = "sideId", description = "선택한 카드가 위치한 side의 id", in = ParameterIn.PATH) @PathVariable Long sideId,
+            @Parameter(name = "cardId", description = "작업자를 추가할 card의 id", in = ParameterIn.PATH) @PathVariable Long cardId,
+            @Parameter(name = "requestDto", description = "초대할 작업자의 정보 (email)") @RequestBody InviteCardRequestDto requestDto
+    ) {
         return cardService.addWorker(boardId, sideId, cardId, requestDto.getEmail());
     }
 
-    // 카드 이름 수정
     @PutMapping("/{boardId}/sides/{sideId}/cards/{cardId}/name")
-    public ResponseEntity<CardResponseDto> updateName(@PathVariable Long boardId, @PathVariable Long sideId, @PathVariable Long cardId,
-                                                      @RequestBody NameRequestDto requestDto) {
+    @Operation(summary = "카드 이름 수정", description = "@PathVariable을 통해 boardId, sideId, cardId를 받아와, 해당 정보와 일치하는 카드에 Dto로부터 받아온 name(이름) 정보를 수정합니다.")
+    public ResponseEntity<CardResponseDto> updateName(
+            @Parameter(name = "boardId", description = "선택한 카드가 위치한 board의 id", in = ParameterIn.PATH) @PathVariable Long boardId,
+            @Parameter(name = "sideId", description = "선택한 카드가 위치한 side의 id", in = ParameterIn.PATH) @PathVariable Long sideId,
+            @Parameter(name = "cardId", description = "이름을 수정할 card의 id", in = ParameterIn.PATH) @PathVariable Long cardId,
+            @Parameter(name = "requestDto", description = "카드 이름 수정 정보") @RequestBody NameRequestDto requestDto
+    ) {
         return cardService.updateName(boardId, sideId, cardId, requestDto.getName());
     }
 
-    // 카드 설명 수정
     @PutMapping("/{boardId}/sides/{sideId}/cards/{cardId}/desc")
-    public ResponseEntity<CardResponseDto> updateDesc(@PathVariable Long boardId, @PathVariable Long sideId, @PathVariable Long cardId,
-                                                      @RequestBody DescRequestDto requestDto) {
+    @Operation(summary = "카드 설명 수정", description = "@PathVariable을 통해 boardId, sideId, cardId를 받아와, 해당 정보와 일치하는 카드에 Dto로부터 받아온 description(설명) 정보를 수정합니다.")
+    public ResponseEntity<CardResponseDto> updateDesc(
+            @Parameter(name = "boardId", description = "선택한 카드가 위치한 board의 id", in = ParameterIn.PATH) @PathVariable Long boardId,
+            @Parameter(name = "sideId", description = "선택한 카드가 위치한 side의 id", in = ParameterIn.PATH) @PathVariable Long sideId,
+            @Parameter(name = "cardId", description = "설명을 수정할 card의 id", in = ParameterIn.PATH) @PathVariable Long cardId,
+            @Parameter(name = "requestDto", description = "카드 설명 수정 정보") @RequestBody DescRequestDto requestDto
+    ) {
         return cardService.updateDesc(boardId, sideId, cardId, requestDto.getDescription());
     }
 
-    // 카드 색상 수정
     @PutMapping("/{boardId}/sides/{sideId}/cards/{cardId}/color")
-    public ResponseEntity<CardResponseDto> updateColor(@PathVariable Long boardId, @PathVariable Long sideId, @PathVariable Long cardId,
-                                                       @RequestBody ColorRequestDto requestDto) {
+    @Operation(summary = "카드 색상 수정", description = "@PathVariable을 통해 boardId, sideId, cardId를 받아와, 해당 정보와 일치하는 카드에 Dto로부터 받아온 color(색상) 정보를 수정합니다.")
+    public ResponseEntity<CardResponseDto> updateColor(
+            @Parameter(name = "boardId", description = "선택한 카드가 위치한 board의 id", in = ParameterIn.PATH) @PathVariable Long boardId,
+            @Parameter(name = "sideId", description = "선택한 카드가 위치한 side의 id", in = ParameterIn.PATH) @PathVariable Long sideId,
+            @Parameter(name = "cardId", description = "색상을 수정할 card의 id", in = ParameterIn.PATH) @PathVariable Long cardId,
+            @Parameter(name = "requestDto", description = "카드 색상 수정 정보") @RequestBody ColorRequestDto requestDto
+    ) {
         return cardService.updateColor(boardId, sideId, cardId, requestDto.getColor());
     }
 
-    // 카드 마감 기한 설정 및 수정
     @PutMapping("/{boardId}/sides/{sideId}/cards/{cardId}/due")
-    public ResponseEntity<CardResponseDto> updateDue(@PathVariable Long boardId, @PathVariable Long sideId, @PathVariable Long cardId,
-                                                      @RequestBody DueRequestDto requestDto) {
+    @Operation(summary = "카드 마감 기한 설정 및 수정", description = "@PathVariable을 통해 boardId, sideId, cardId를 받아와, 해당 정보와 일치하는 카드에 Dto로부터 받아온 due(마감기한) 정보를 수정합니다.")
+    public ResponseEntity<CardResponseDto> updateDue(
+            @Parameter(name = "boardId", description = "선택한 카드가 위치한 board의 id", in = ParameterIn.PATH) @PathVariable Long boardId,
+            @Parameter(name = "sideId", description = "선택한 카드가 위치한 side의 id", in = ParameterIn.PATH) @PathVariable Long sideId,
+            @Parameter(name = "cardId", description = "마감 기한을 설정 및 수정할 card의 id", in = ParameterIn.PATH) @PathVariable Long cardId,
+            @Parameter(name = "requestDto", description = "카드 마감 기한 설정 및 수정에 대한 정보 (LocalDateTime)") @RequestBody DueRequestDto requestDto
+    ) {
         return cardService.updateDue(boardId, sideId, cardId, requestDto);
     }
 
-    // 카드 마감 기한 삭제
     @PutMapping("/{boardId}/sides/{sideId}/cards/{cardId}/due-removal")
-    public ResponseEntity<CardResponseDto> updateDue(@PathVariable Long boardId, @PathVariable Long sideId, @PathVariable Long cardId) {
+    @Operation(summary = "카드 마감 기한 삭제", description = "@PathVariable을 통해 boardId, sideId, cardId를 받아와, 해당 정보와 일치하는 카드의 due(마감기한) 정보를 삭제합니다.")
+    public ResponseEntity<CardResponseDto> updateDue(
+            @Parameter(name = "boardId", description = "선택한 카드가 위치한 board의 id", in = ParameterIn.PATH) @PathVariable Long boardId,
+            @Parameter(name = "sideId", description = "선택한 카드가 위치한 side의 id", in = ParameterIn.PATH) @PathVariable Long sideId,
+            @Parameter(name = "cardId", description = "마감 기한을 삭제시킬 card의 id", in = ParameterIn.PATH) @PathVariable Long cardId
+    ) {
         return cardService.updateDueRemoval(boardId, sideId, cardId);
     }
 
-    // 카드 이동
     @PutMapping("/{boardId}/sides/{sideId}/cards/{cardId}/position")
-    public ResponseEntity<CardResponseDto> moveCard(@PathVariable Long boardId, @PathVariable Long sideId, @PathVariable Long cardId,
-                                                    @RequestBody MoveRequestDto requestDto ) {
+    @Operation(summary = "카드 이동", description = "@PathVariable을 통해 boardId, sideId, cardId를 받아와, 해당 정보와 일치하는 카드의 position(순서)를 Dto로부터 받아온 position 정보를 사용하여 수정합니다.")
+    public ResponseEntity<CardResponseDto> moveCard(
+            @Parameter(name = "boardId", description = "선택한 카드가 위치한 board의 id", in = ParameterIn.PATH) @PathVariable Long boardId,
+            @Parameter(name = "sideId", description = "선택한 카드가 위치한 side의 id", in = ParameterIn.PATH) @PathVariable Long sideId,
+            @Parameter(name = "cardId", description = "이동시킬 card의 id", in = ParameterIn.PATH) @PathVariable Long cardId,
+            @Parameter(name = "requestDto", description = "카드를 이동시킬 정보 (position)") @RequestBody MoveRequestDto requestDto) {
         return cardService.moveCard(boardId, sideId, cardId, requestDto);
     }
-    
-    // 카드 삭제
+
     @DeleteMapping("/{boardId}/sides/{sideId}/cards/{cardId}")
-    public ResponseEntity<ApiResponseDto> deleteCard(@PathVariable Long boardId, @PathVariable Long sideId, @PathVariable Long cardId,
-                           @AuthenticationPrincipal UserDetailsImpl userDetails) {
+    @Operation(summary = "카드 삭제", description = "@PathVariable을 통해 boardId, sideId, cardId를 받아와, 해당 정보와 일치하는 카드를 삭제합니다.")
+    public ResponseEntity<ApiResponseDto> deleteCard(
+            @Parameter(name = "boardId", description = "선택한 카드가 위치한 board의 id", in = ParameterIn.PATH) @PathVariable Long boardId,
+            @Parameter(name = "sideId", description = "선택한 카드가 위치한 side의 id", in = ParameterIn.PATH) @PathVariable Long sideId,
+            @Parameter(name = "cardId", description = "삭제시킬 card의 id", in = ParameterIn.PATH) @PathVariable Long cardId,
+            @AuthenticationPrincipal UserDetailsImpl userDetails) {
         return cardService.deleteCard(boardId, sideId, cardId, userDetails.getUser());
     }
 }
