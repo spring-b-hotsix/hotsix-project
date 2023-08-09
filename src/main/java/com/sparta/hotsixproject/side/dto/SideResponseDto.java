28afd5a05e8fee84fdfd2fe82c54c9a58ea988c3
jungeun-choi <jungeun5.choi@gmail.com>
2023-08-09 10:22:31 +0900

    [Update] SideResponseDto에 cardList 추가

--- src/main/java/com/sparta/hotsixproject/side/dto/SideResponseDto.java
+++ src/main/java/com/sparta/hotsixproject/side/dto/SideResponseDto.java
@@ -1,8 +1,12 @@
 package com.sparta.hotsixproject.side.dto;
 
+import com.sparta.hotsixproject.card.dto.CardResponseDto;
 import com.sparta.hotsixproject.side.entity.Side;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Getter
 @NoArgsConstructor
@@ -11,11 +15,15 @@ public class SideResponseDto {
     private String name;
     private int position;
     private Long boardId;
+    private List<CardResponseDto> cardList;
 
     public SideResponseDto(Side side) {
         this.id = side.getId();
         this.name = side.getName();
         this.position = side.getPosition();
         this.boardId = side.getBoard().getId();
+        this.cardList = side.getCardList().stream()
+                .map(CardResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
