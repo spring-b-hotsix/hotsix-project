4445c501a41a4de3fdc14f70e83a88003483412c
Jo Woojin <dnwls3525@gmail.com>
2023-08-10 12:56:47 +0900

    [Update] HomeController 설명추가

--- src/main/java/com/sparta/hotsixproject/common/controller/HomeController.java
+++ src/main/java/com/sparta/hotsixproject/common/controller/HomeController.java
@@ -3,6 +3,7 @@ package com.sparta.hotsixproject.common.controller;
 import com.sparta.hotsixproject.board.dto.BoardResponseDto;
 import com.sparta.hotsixproject.board.service.BoardService;
 import com.sparta.hotsixproject.common.security.UserDetailsImpl;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -18,6 +19,7 @@ public class HomeController {
         this.boardService = boardService;
     }
     @GetMapping("/")
+    @Operation(summary = "메인페이지", description = "index.html페이지로 이동 및 내 보드, 내가 참여한 보드 정보를 보여줍니다.")
     public String goHome(@AuthenticationPrincipal UserDetailsImpl userDetails, Model model ){
         String nickname = userDetails.getUsername();
         //내가 만든 보드, 내가 참여한 보드 불러오기
