08f6c92a8032fe21eb59584c3b022733a61b384b
jungeun-choi <jungeun5.choi@gmail.com>
2023-08-11 17:25:27 +0900

    [Update] User: 회원가입 실패(assertThrow 추가), 회원 정보 수정(nickname, password)

--- src/test/java/com/sparta/hotsixproject/user/service/UserServiceTest.java
+++ src/test/java/com/sparta/hotsixproject/user/service/UserServiceTest.java
@@ -1,7 +1,8 @@
 package com.sparta.hotsixproject.user.service;
 
-import com.sparta.hotsixproject.user.UserRoleEnum;
 import com.sparta.hotsixproject.user.dto.LoginRequestDto;
+import com.sparta.hotsixproject.user.dto.UpdateNicknameRequestDto;
+import com.sparta.hotsixproject.user.dto.UpdatePasswordRequestDto;
 import com.sparta.hotsixproject.user.entity.User;
 import com.sparta.hotsixproject.user.repository.UserRepository;
 import jakarta.persistence.EntityManager;
@@ -11,12 +12,12 @@ import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 
 @RunWith(SpringRunner.class)
@@ -37,7 +38,7 @@ class UserServiceTest {
     void signupSuccess() {
         // given
         String nickname = "test1";
-        String email = "test1234@email.com";
+        String email = "test1@email.com";
         String encodePw = passwordEncoder.encode("Password@1234");
         LoginRequestDto requestDto = new LoginRequestDto(nickname, encodePw, email);
         userService.signup(requestDto);
@@ -51,32 +52,63 @@ class UserServiceTest {
 
     @Test
     @DisplayName("회원가입 실패")
-    void signupFail() throws Exception {
+    void signupFail() throws IllegalArgumentException {
         // given
-        String nickname = "test1";
-        String email = "test1234@email.com";
+        String nickname = "test2";
+        String email = "test2@email.com";
         String encodePw = passwordEncoder.encode("Password@1234");
-        LoginRequestDto requestDto1 = new LoginRequestDto(nickname, email, encodePw);
+        LoginRequestDto requestDto1 = new LoginRequestDto(nickname, encodePw, email);
         userService.signup(requestDto1);
 
-        nickname = "test2";
+        nickname = "test3";
         /* email = 이미 가입한 email 사용 */
         encodePw = passwordEncoder.encode("Password@1234");
-        LoginRequestDto requestDto2 = new LoginRequestDto(nickname, email, encodePw);
-        userService.signup(requestDto2);
+        LoginRequestDto requestDto2 = new LoginRequestDto(nickname, encodePw, email);
 
         // when
-        User newUser1 = userRepository.findByEmail(email).orElse(null);
-        User newUser2 = userRepository.findByEmail(email).orElse(null);
+        IllegalArgumentException exception = assertThrows( // 동일한 이메일로 회원가입 시 예외 throw
+                IllegalArgumentException.class, () -> {
+                    userService.signup(requestDto2);
+                }
+        );
+        User newUser1 = userRepository.findByNickname("test2").orElse(null);
+        User newUser2 = userRepository.findByNickname("test3").orElse(null);
 
         // then
-        assertEquals(nickname, newUser1.getNickname()); // user1 = 회원가입 완료
+        assertEquals("test2", newUser1.getNickname()); // user1 = 회원가입 완료
+        assertEquals("이미 존재하는 이메일입니다.", exception.getMessage()); // user2 가입 시 예외 throw
         assertNull(newUser2); // user2 = 회원가입 실패
     }
 
     @Test
     @DisplayName("회원 정보 수정")
     void updateNicknameAndPassword() {
+        // given
+        String nickname = "test4";
+        String email = "test4@email.com";
+        String encodePw = passwordEncoder.encode("Password@1234");
+        LoginRequestDto signupDto = new LoginRequestDto(nickname, encodePw, email);
 
+        userService.signup(signupDto);
+        User newUser = userRepository.findByEmail(email).orElse(null);
+
+        // when
+        /* 1. 닉네임 변경 */
+        nickname = "newnick1";
+        UpdateNicknameRequestDto nicknameDto = new UpdateNicknameRequestDto(nickname, signupDto.getPassword());
+        userService.updateNickname(newUser.getId(), nicknameDto, newUser);
+
+        /* 2. 비밀번호 변경 */
+        encodePw = passwordEncoder.encode("newPass@1234");
+        UpdatePasswordRequestDto pwDto = new UpdatePasswordRequestDto(signupDto.getPassword(), encodePw);
+        userService.updatePassword(newUser.getId(), pwDto, newUser);
+
+        // then
+        /* 1. 닉네임 비교 */
+        assertEquals("newnick1", newUser.getNickname());
+        /* 2. 비밀번호 비교 */
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        boolean match = encoder.matches(encodePw, newUser.getPassword()); // 비교
+        assertTrue(match);
     }
 }
