96a080d93b7dd7165541aeb19aa6e8ca1a7d1fbe
hyukjin <cdh5771@naver.com>
2023-08-14 03:24:06 +0900

    Revert "Revert "[Refactor] 알람 전체 조회에 board에 대한 정보 추가""
    This reverts commit 01109af12ae0ad8cc305a5afe2fc0340e0bf962b.

--- src/main/java/com/sparta/hotsixproject/boarduser/repository/BoardUserRepository.java
+++ src/main/java/com/sparta/hotsixproject/boarduser/repository/BoardUserRepository.java
@@ -11,6 +11,9 @@ import java.util.Optional;
 
 @Repository
 public interface BoardUserRepository extends JpaRepository<BoardUser, Long> {
+    boolean existsByBoard_IdAndUser_Id(Long boardId, Long userId);
+    boolean existsByUser_Id(Long id);
+    BoardUser findByUser_Id(Long id);
     Optional<BoardUser> findByUser_EmailAndBoard_Id(String email, Long Id);
     List<BoardUser> findByBoard_Id(Long Id);
     List<BoardUser> findAllByUser(User user);
