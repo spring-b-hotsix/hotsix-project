b3d5933448a58b9bcc726ad3cf39a1f30d050064
jungeun-choi <jungeun5.choi@gmail.com>
2023-08-14 02:40:24 +0900

    [Update] DummyGenerator: Comment, Label 더미 데이터 추가

--- src/main/java/com/sparta/hotsixproject/common/dummy/DummyGenerator.java
+++ src/main/java/com/sparta/hotsixproject/common/dummy/DummyGenerator.java
@@ -7,6 +7,10 @@ import com.sparta.hotsixproject.boarduser.entity.BoardUser;
 import com.sparta.hotsixproject.boarduser.repository.BoardUserRepository;
 import com.sparta.hotsixproject.card.entity.Card;
 import com.sparta.hotsixproject.card.repository.CardRepository;
+import com.sparta.hotsixproject.comment.dto.CommentRequestDto;
+import com.sparta.hotsixproject.comment.entity.Comment;
+import com.sparta.hotsixproject.comment.repository.CommentRepository;
+import com.sparta.hotsixproject.label.entity.Label;
 import com.sparta.hotsixproject.label.repository.LabelRepository;
 import com.sparta.hotsixproject.side.entity.Side;
 import com.sparta.hotsixproject.side.repository.SideRepository;
@@ -29,16 +33,18 @@ public class DummyGenerator implements CommandLineRunner {
     private final BoardUserRepository boardUserRepository;
     private final SideRepository sideRepository;
     private final CardRepository cardRepository;
+    private final CommentRepository commentRepository;
     private final LabelRepository labelRepository;
 
     // 상수
-    private static final int COUNT = 5;
+    private static final int COUNT = 3;
 
     @Autowired
     public DummyGenerator(
             PasswordEncoder passwordEncoder,
             UserRepository userRepository, BoardRepository boardRepository, BoardUserRepository boardUserRepository,
-            SideRepository sideRepository, CardRepository cardRepository, LabelRepository labelRepository
+            SideRepository sideRepository, CardRepository cardRepository, CommentRepository commentRepository,
+            LabelRepository labelRepository
     ) {
         this.faker = new Faker();
         this.passwordEncoder = passwordEncoder;
@@ -47,16 +53,28 @@ public class DummyGenerator implements CommandLineRunner {
         this.boardUserRepository = boardUserRepository;
         this.sideRepository = sideRepository;
         this.cardRepository = cardRepository;
+        this.commentRepository = commentRepository;
         this.labelRepository = labelRepository;
     }
 
     // 실행
     @Override
     public void run(String... args) throws Exception {
+        // 사용자
         List<User> userList = DummyUserGenerator();
+        // 보드
         Map<User, Board> boardMap = DummyBoardGenerator(userList);
+        // 컬럼
         Map<Board, List<Side>> sideListMap = DummySideGenerator(userList, boardMap);
+        // 카드
         Map<Side, List<Card>> cardListMap = DummyCardGenerator(userList, boardMap, sideListMap);
+        // 댓글 - 전체 중 한 개의 카드에만 생성함
+        Map<Card, List<Comment>> commentListMap = DummyCommentGenerator(userList, boardMap, sideListMap, cardListMap);
+        // 라벨
+        List<Label> labelList = DummyLabelGenerator(userList, boardMap);
+        // 체크리스트 - 전체 중 한 개의 카드에만 생성함
+
+        // 작업자 초대 - 전체 중 한 개의 카드에만 생성함
     }
 
     // User
@@ -95,8 +113,8 @@ public class DummyGenerator implements CommandLineRunner {
     // Side(컬럼)
     private Map<Board, List<Side>> DummySideGenerator(List<User> userList, Map<User, Board> boardMap) {
         Map<Board, List<Side>> sideListMap = new HashMap<>();
-        List<Side> sideList = new ArrayList<>();
         for (int i = 0; i < COUNT; i++) {
+            List<Side> sideList = new ArrayList<>();
             User user = userList.get(i);
             Board board = boardMap.get(user);
 
@@ -124,27 +142,66 @@ public class DummyGenerator implements CommandLineRunner {
             Board board = boardMap.get(user);
             List<Side> sideList = sideListMap.get(board);
 
-            for(int m = 0; m < sideList.size(); m++) {
-                List<Card> cardList = new ArrayList<>();
-                Card card1 = new Card("card1", m, user, sideList.get(m));
-                Card card2 = new Card("card2", m, user, sideList.get(m));
-                Card card3 = new Card("card3", m, user, sideList.get(m));
+            List<Card> cardList = new ArrayList<>();
+            for (int m = 0; m < sideList.size(); m++) { // 이거 다시 써야댐
+                Card card = new Card("card" + m, m, user, sideList.get(i));
+                cardRepository.save(card);
 
-                cardRepository.save(card1);
-                cardRepository.save(card2);
-                cardRepository.save(card3);
-
-                cardList.add(card1);
-                cardList.add(card2);
-                cardList.add(card3);
-                cardListMap.put(sideList.get(m), cardList);
+                cardList.add(card);
             }
+            cardListMap.put(sideList.get(i), cardList);
         }
         return cardListMap;
     }
 
+    // Comment
+    private Map<Card, List<Comment>> DummyCommentGenerator(List<User> userList, Map<User, Board> boardMap, Map<Board, List<Side>> sideListMap, Map<Side, List<Card>> cardListMap) {
+        Map<Card, List<Comment>> commentListMap = new HashMap<>();
+        List<Comment> commentList = new ArrayList<>();
 
+        User user = userList.get(0);
+        Board board = boardMap.get(user);
+        Side side = sideListMap.get(board).get(0);
+        Card card = cardListMap.get(side).get(0);
 
+        List<String> contentList = new ArrayList<>();
+        contentList.add("할 일이 많네요");
+        contentList.add("힘냅시다~");
+
+        for (int i = 0; i < contentList.size(); i++) {
+            CommentRequestDto requestDto = new CommentRequestDto(contentList.get(i));
+            Comment comment = new Comment(card, requestDto, user);
+
+            commentRepository.save(comment);
+            commentList.add(comment);
+        }
+        commentListMap.put(card, commentList);
+
+        return commentListMap;
+    }
 
     // Card 세부 (라벨, 체크리스트, 작업자 등)
+    // 1. 라벨
+    private List<Label> DummyLabelGenerator(List<User> userList, Map<User, Board> boardMap) {
+        List<Label> labelList = new ArrayList<>();
+        for (int i = 0; i < COUNT; i++) {
+            User user = userList.get(i);
+            Board board = boardMap.get(user);
+            String title = "label" + i;
+
+            int red = (int) (Math.random() * 256);
+            int green = (int) (Math.random() * 256);
+            int blue = (int) (Math.random() * 256);
+
+            Label label = new Label(board, title, red, green, blue);
+
+            labelRepository.save(label);
+            labelList.add(label);
+        }
+        return labelList;
+    }
+
+    // 2. 체크리스트
+
+    // 3. 작업자 초대
 }
